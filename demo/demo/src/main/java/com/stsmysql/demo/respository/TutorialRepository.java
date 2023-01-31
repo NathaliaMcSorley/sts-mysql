807add54867fce4910921e30263195b030984902
Nathalia <nathaliamcsorley@gmail.com>
2023-01-30 19:48:15 -0600

    code

---
+++ demo/demo/src/main/java/com/stsmysql/demo/respository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.stsmysql.demo.respository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.stsmysql.demo.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+}
